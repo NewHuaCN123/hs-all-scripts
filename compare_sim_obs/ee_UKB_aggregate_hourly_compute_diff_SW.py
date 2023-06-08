fad3215978d79c9e886d624efffeb71ae811e9c2
Tadesse <michaelg.tadesse@gmail.com>
2023-06-08 13:44:04 -0400

    automated stat output

--- compare_sim_obs/ee_UKB_aggregate_hourly_compute_diff_SW.py
+++ compare_sim_obs/ee_UKB_aggregate_hourly_compute_diff_SW.py
@@ -10,75 +10,92 @@ from sklearn.metrics import mean_absolute_error
 
 
 obs_dir = "R:\\40715-013 UKFPLOS\\Data\\H&H_Data\\calibration_validation_stats\\"\
-                    "cal_0606\\flow_stats\\obs_flows"
+                    "cal_0607\\flow_stats\\obs_flows"
 sim_dir = "R:\\40715-013 UKFPLOS\\Data\\H&H_Data\\calibration_validation_stats\\"\
-                    "cal_0606\\flow_stats\\sim_flows"
+                    "cal_0607\\flow_stats\\sim_flows"
 out_dir = "R:\\40715-013 UKFPLOS\\Data\\H&H_Data\\calibration_validation_stats\\"\
-                    "cal_0606\\flow_stats\\merged_flow"
+                    "cal_0607\\flow_stats\\merged_flow"
 
 
 os.chdir(obs_dir)
 
-station = "kub001"
-obs = pd.read_csv('{}.csv'.format(station))
-# print(obs)
-obs['datetime'] = pd.to_datetime(obs['datetime'])
-
-print(obs)
-
-os.chdir(sim_dir)
-sim = pd.read_csv('Cal_0606DetailedTS_M11.csv')
-sim['datetime'] = pd.to_datetime(sim['datetime'])
+# list of stations
+sList = ['g113','s65A',	'g103',	
+         's57',	's58',	's59',	
+         's60',	's61',	's62',	
+         's63',	's63A',	's65',	
+         'shingle_creek','reedy_creek',	
+         'boggy_creek',	'kub001']
 
 
-# # consider only data past '2017-09-10'
-# # but for short period analysis do 09/10 - 09/18
+for station in sList:
+    os.chdir(obs_dir)
+    # print(station)
 
-## Cal - Long Run
-# sim = sim[(sim['datetime'] >= '2017-09-10') & (sim['datetime'] <= '2017-10-10')]
+    obs = pd.read_csv('{}.csv'.format(station))
+    # print(obs)
+    obs['datetime'] = pd.to_datetime(obs['datetime'])
 
-# ## Val - Long Run
-# sim = sim[(sim['datetime'] >= '2011-09-29') & (sim['datetime'] <= '2011-10-27')]
+    # print(obs)
 
-# # Cal - Storm Event Period
-sim = sim[(sim['datetime'] >= '2017-09-10') & (sim['datetime'] <= '2017-09-18')]
-
-## Val - Storm Event Period
-# sim = sim[(sim['datetime'] >= '2011-10-03') & (sim['datetime'] <= '2011-10-27')]
-
-sim = sim[['datetime', station + "_sim"]]
-print(sim)
-
-# convert CMS to CFS
-sim[station + "_sim"] = sim[station + "_sim"]*35.314666212661
-print(sim)
-
-obs.set_index(obs['datetime'], inplace = True)
-obs_agg = pd.DataFrame(obs.iloc[:,1].resample('H').mean())
-
-print(obs_agg)
-
-# aggregate sim timeseries
-sim.set_index(sim['datetime'], inplace = True)
-sim_agg = pd.DataFrame(sim.iloc[:,1].resample('H').mean())
-print(sim_agg)
-
-# merge obs and sim
-dat_merged = pd.merge(sim_agg, obs_agg, on = 'datetime', how = 'inner')
-
-dat_merged.reset_index(inplace = True)
-dat_merged.dropna(inplace = True)
-print(dat_merged)
+    os.chdir(sim_dir)
+    sim = pd.read_csv('Cal_0607DetailedTS_M11.csv')
+    sim['datetime'] = pd.to_datetime(sim['datetime'])
 
 
-# calculate calibration target
-dat_merged.columns = ['datetime', 'sim', 'obs']
-dat_merged['good_criteria'] = (dat_merged['sim'] >= 0.80*dat_merged['obs']) & (dat_merged['sim'] <= 1.2*dat_merged['obs']) 
+    # # consider only data past '2017-09-10'
+    # # but for short period analysis do 09/10 - 09/18
+
+    ## Cal - Long Run
+    # sim = sim[(sim['datetime'] >= '2017-09-10') & (sim['datetime'] <= '2017-10-10')]
+
+    # ## Val - Long Run
+    # sim = sim[(sim['datetime'] >= '2011-09-29') & (sim['datetime'] <= '2011-10-27')]
+
+    # # Cal - Storm Event Period
+    sim = sim[(sim['datetime'] >= '2017-09-10') & (sim['datetime'] <= '2017-09-18')]
+
+    ## Val - Storm Event Period
+    # sim = sim[(sim['datetime'] >= '2011-10-03') & (sim['datetime'] <= '2011-10-27')]
+
+    sim = sim[['datetime', station + "_sim"]]
+    # print(sim)
+
+    # convert CMS to CFS
+    sim[station + "_sim"] = sim[station + "_sim"]*35.314666212661
+    # print(sim)
+
+    obs.set_index(obs['datetime'], inplace = True)
+    obs_agg = pd.DataFrame(obs.iloc[:,1].resample('H').mean())
+
+    # print(obs_agg)
+
+    # aggregate sim timeseries
+    sim.set_index(sim['datetime'], inplace = True)
+    sim_agg = pd.DataFrame(sim.iloc[:,1].resample('H').mean())
+    # print(sim_agg)
+
+    # merge obs and sim
+    dat_merged = pd.merge(sim_agg, obs_agg, on = 'datetime', how = 'inner')
+
+    dat_merged.reset_index(inplace = True)
+    dat_merged.dropna(inplace = True)
+    # print(dat_merged)
 
 
-# count TRUE 
-dat_merged['good_perc'] = len(dat_merged[dat_merged['good_criteria'] == True])/len(dat_merged)
-print(dat_merged)
+    # check length of dat_merged
+    if len(dat_merged) == 0:
+        print(station + " - No Data")
+        continue
+
+    # calculate calibration target
+    dat_merged.columns = ['datetime', 'sim', 'obs']
+    dat_merged['good_criteria'] = (dat_merged['sim'] >= 0.80*dat_merged['obs']) & (dat_merged['sim'] <= 1.2*dat_merged['obs']) 
+
+
+    # count TRUE 
+    dat_merged['good_perc'] = len(dat_merged[dat_merged['good_criteria'] == True])/len(dat_merged)
+    print(station, "  -  ", dat_merged['good_perc'][0])
 
 
 
