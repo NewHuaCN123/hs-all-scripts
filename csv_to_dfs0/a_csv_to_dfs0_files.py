3ba85a75ddc4e32226c05956f498a35625971577
Tadesse <michaelg.tadesse@gmail.com>
2023-09-15 08:25:40 -0400

    dfs0 conversion tool

--- csv_to_dfs0/a_csv_to_dfs0_files.py
+++ csv_to_dfs0/a_csv_to_dfs0_files.py
@@ -4,6 +4,12 @@ Modified on Fri Aug 04 17:06:00 2023
 
 create dfs0 files
 Input files = water level csv files
+
+Details on EUM
+https://dhi.github.io/mikeio/eum.html
+
+*this script still needs to be modified to include the DataValueType item included
+
 @author: Michael Getachew Tadesse
 
 """
@@ -15,8 +21,8 @@ import mikeio
 from mikeio.eum import ItemInfo, EUMType, EUMUnit
 from mikecore.DfsFile import DataValueType
 
-dir_in = "R:\\KB_USACE\\Data\\data_from_dbhydro\\GW\\GW Cleaned"
-dir_out = "R:\\KB_USACE\\Data\\data_from_dbhydro\\GW\\gw_stage_dfs0"
+dir_in = "O:\\40715-HWD\\40715-021\\Modeling\\Data\\rainfall\\5y\\concat_rain"
+dir_out = "O:\\40715-HWD\\40715-021\\Modeling\\Data\\rainfall\\5y\\concat_rain"
 
 
 
@@ -24,7 +30,7 @@ os.chdir(dir_in)
 
 pixList = os.listdir()
 
-pixList = ['S58_T.csv']
+pixList = ['combined.csv']
 
 for pp in pixList:
     print(pp)
@@ -32,23 +38,14 @@ for pp in pixList:
     os.chdir(dir_in)
 
     df = pd.read_csv(pp, parse_dates=True, 
-                index_col='Daily Date', na_values=-99.99)
-    df = df[['Data Value']]
+                index_col='datetime', na_values=-99.99)
+    
+    # df = df[df.columns[1:]]
+
+    # remove the last row
     df = df.head(df.shape[0] -1)
-
-    print(df)
-
-    item = ItemInfo(EUMType.Water_Level, EUMUnit.feet, data_value_type = DataValueType.Instantaneous)
-
-    da = mikeio.DataArray(df['Data Value'], time = df.index, item = item)
-
-    ds = mikeio.Dataset([da])
 
     os.chdir(dir_out)
 
-    # find standard pixel id
-    saveName = pp + ".dfs0"
-
-    # print(pp, saveName)
-
-    ds.to_dfs(saveName)
+    df.to_dfs0("Rain_5y_v2.dfs0", EUMType.Rainfall, EUMUnit.inch)
+    
